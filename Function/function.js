85438ab0758fcc18c703dada219797dbfd93c534
shubham sheladiya <sheladiyashubham017@gmail.com>
2024-10-16 22:53:56 +0530

    15-10-function

--- Function/function.js
+++ Function/function.js
@@ -73,11 +73,75 @@
 // }
 // small();
 
-// 7.find out given number is even or odd.
+// 7.find out given number is even 
+// function even(){
+//     let e=Number(prompt('Enter the Number:'));
+//     if (e % 2===0) {
+//         console.log('This number is even:',e);
+//     }else{
+//         console.log('This number is  odd:',e);
+//     }
+// }   
+// even();
+// expected output:- 10 =>even number
+
+// 8.find out given number is  odd
+// function odd(){
+//     let o=Number(prompt('Enter the Number:'));
+//     if (o % 2===1) {
+//         console.log('This number is odd:',o);
+//     }else{
+//         console.log('This number is even:',o);
+//     }
+// }
+// odd();
+// out-put:- 3 => odd number
+
+// 9.Convert Celsius to Fahrenheit.
+// function Fahrenheit(){
+// let c=Number(prompt('Enter the Celsius'));
+//  let f = c*1.8+32;
+//  console.log('Fahrenheit:',f);
+// }
+// Fahrenheit();
+// expected output:-user= 10 => 50.
+
+// 10.Function to Find the Maximum of Three Numbers.
+// function Max(){
+//     let a=Number(prompt('Enter the A Number:'));
+//     let b=Number(prompt('Enter the B Number:'));
+//     let c=Number(prompt('Enter the C Number:'));
+//     if(a>b)
+//         {
+//             if(a>c)
+//             {
+//                 console.log("A number is Maximum",a);
+//             }
+//             else
+//             {
+//                 console.log("C number is Maximum",c);
+//             }
+//         }
+//         else
+//         {
+//             if(b>c)
+//             {
+//                 console.log("B number is Maximum",b);
+//             }
+//             else
+//             {
+//                 console.log("C number is Maximum",c);
+//             }
+//         }
+// }
+// Max();
+// expected output:- user= a=12,b=15,c=19 => c number is maximum.
 
 
 
 
+
+  
 
 
 
